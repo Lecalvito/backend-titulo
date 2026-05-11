659b3b0e05cbb6f901d6ae6e8a3c505036ae5699
Lecalvito <axel.brantmarin@gmail.com>
2026-05-11 17:39:53 -0400

    Update app.js
    Correccion de CORS

--- src/app.js
+++ src/app.js
@@ -5,7 +5,9 @@ const routes = require("./routes/index.routes");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://frontend-titulo-axelbm.netlify.app/'
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
